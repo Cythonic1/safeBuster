035f1f3868bc71b848055b76f1f3486e4e80b2f5
Cythonic1 <jfjbn80@gmail.com>
2025-03-11 07:04:55 +0800

    Finish parsing headers and adding more args

--- src/buster/safebuster.rs
+++ src/buster/safebuster.rs
@@ -1,29 +1,80 @@
-use std::{collections::HashMap, env::args};
+use std::{collections::HashMap};
 
-use reqwest::{blocking::Client};
+use reqwest::{blocking::Client, header::HeaderMap, header::HeaderName, header::HeaderValue};
+
 use super::cli;
 
-fn init_headers() -> Vec<String> {
-    vec![
-        "User-Agent: SafeBuster/1.0".to_string(),
-        "Accept: */*".to_string(),
-    ]
+fn init_headers_with_defaults() -> HeaderMap {
+    let mut hash = HeaderMap::new();
+
+    let headers = vec![
+        "Content-Type: application/json",
+        "Accept: application/json",
+        "Authorization: Bearer <token>",
+        "User-Agent: SafeBuster/1.0",
+        "Accept: */*",
+    ];
+
+    headers.iter().for_each(|header| {
+        let mut parts = header.splitn(2, ':');
+        if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
+            let key = key.trim();
+            let value = value.trim();
+
+            if let Ok(header_name) = HeaderName::from_bytes(key.as_bytes()) {
+                if let Ok(header_value) = HeaderValue::from_str(value) {
+                    hash.insert(header_name, header_value);
+                }
+            }
+        }
+    });
+
+    hash
 }
 
-fn prepare_headers(headers: &mut Option<Vec<String>>) {
-    match headers {
-        Some(header) => {
-            println!("Existing headers: {:?}", header);
-        }
-        None => {
-            println!("Headers are empty. Initializing...");
-            *headers = Some(init_headers());
+fn init_headers_with_value(headers: Vec<String>) -> HeaderMap {
+    let mut hash = HeaderMap::new();
+
+    for header in headers {
+        let mut parts = header.splitn(2, ':');
+        if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
+            let key = key.trim();
+            let value = value.trim();
+
+            // Convert key and value into HeaderName and HeaderValue
+            if let Ok(header_name) = HeaderName::from_bytes(key.as_bytes()) {
+                if let Ok(header_value) = HeaderValue::from_str(value) {
+                    hash.insert(header_name, header_value);
+                }
+            }
         }
     }
+
+    hash
 }
 
-pub fn safe_buster(mut args: cli::Args) {
+fn prepare_headers(headers: Option<Vec<String>>) -> HeaderMap{
+    let  headers_hash;
+    if let Some(header) = headers{
+        headers_hash = init_headers_with_value(header);
+        return headers_hash;
+
+    }else{
+        headers_hash = init_headers_with_defaults();
+        return headers_hash;
+    }
+}
+fn craft_request(args: cli::Args, headers_hash: HeaderMap, client : Client){
+
+    let res = client.get(args.url).headers(headers_hash).send().unwrap();
+    println!("Response : {:#?}", res);
+
+
+}
+pub fn safe_buster( args: cli::Args) {
     let client: Client = Client::new();
-    prepare_headers(&mut args.headers);
-    println!("Hello");
+    let headers = args.headers.clone();
+    let headers_hash = prepare_headers(headers);
+    println!("Headers {:#?}", headers_hash);
+    craft_request(args, headers_hash, client);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,5 @@ use clap::Parser;
 mod buster;
 fn main() {
     let args = buster::cli::Args::parse();
-    let _sometyhiong = buster::safebuster::test();
-    let new = String::from("From Yaser");
-    let _something: i32 = 10;
-    println!("Headers are {:?}", args.headers);
-
-    println!("Hello, world!, {}", new);
+    buster::safebuster::safe_buster(args);
 }
