ecd7c47b1bd2e4004c07f54adf1c13b658d61187
Cythonic1 <jfjbn80@gmail.com>
2025-05-02 03:49:19 +0000

    Take notes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod buster;
 #[tokio::main(flavor = "multi_thread", worker_threads = 20)]
 async fn main() {
     let args = buster::cli::Args::parse();
-    let file = PathBuf::from("/home/groot/Desktop/rust/safebuster/headers_test");
+    let file = PathBuf::from("/home/groot/projects/rust/safeBuster/headers_test");
     let _ = buster::safebuster::parse_file(file, args);
     // let _ = buster::safebuster::safe_buster(args.clone()).await;
     // let _ = buster::safebuster::search_fuzz(args, "Something");
